9cb1670e3a7bd8ba66a1540bc05e3d1239b36f30
dannguyencoder <dannguyencoder@gmail.com>
2019-06-04 17:10:24 +0700

    init commit

---
+++ src/main/java/com/cms/springboot/backend/base/demo/SpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cms.springboot.backend.base.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cms/springboot/backend/base/demo/controller/PostController.java
@@ -0,0 +1,8 @@
+package com.cms.springboot.backend.base.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+public class PostController {
+}
